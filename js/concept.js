aec46d94c43434d0a943e564988154fab637deb1
doiyumika10 <81340931+doiyumika10@users.noreply.github.com>
2021-04-12 00:22:20 +0900

    Add files via upload

---
+++ js/concept.js
@@ -0,0 +1,106 @@
+'use strict';
+
+let btn1 = document.getElementById('btn1');
+let btn2 = document.getElementById('btn2');
+let btn3 = document.getElementById('btn3');
+let btn4 = document.getElementById('btn4');
+
+let content1 = document.getElementById('damageless-article');
+let content2 = document.getElementById('treatment-article');
+let content3 = document.getElementById('headspa-article');
+let content4 = document.getElementById('haircare-article');
+
+let curUrl = location.href;
+let lastUrl = curUrl.slice(-1);
+
+if(lastUrl == 1){
+	btn1.classList.add('active');
+    btn2.classList.remove('active');
+    btn3.classList.remove('active');
+    btn4.classList.remove('active');
+    content1.classList.remove('hidden');
+    content2.classList.add('hidden');
+    content3.classList.add('hidden');
+    content4.classList.add('hidden');
+}else if(lastUrl == 2){
+	btn1.classList.remove('active');
+    btn2.classList.add('active');
+    btn3.classList.remove('active');
+    btn4.classList.remove('active');
+    content1.classList.add('hidden');
+    content2.classList.remove('hidden');
+    content3.classList.add('hidden');
+    content4.classList.add('hidden');
+}else if(lastUrl == 3){
+	btn1.classList.remove('active');
+    btn2.classList.remove('active');
+    btn3.classList.add('active');
+    btn4.classList.remove('active');
+    content1.classList.add('hidden');
+    content2.classList.add('hidden');
+    content3.classList.remove('hidden');
+    content4.classList.add('hidden');
+}else if(lastUrl == 4){
+	btn1.classList.remove('active');
+    btn2.classList.remove('active');
+    btn3.classList.remove('active');
+    btn4.classList.add('active');
+    content1.classList.add('hidden');
+    content2.classList.add('hidden');
+    content3.classList.add('hidden');
+    content4.classList.remove('hidden');
+}else{
+	btn1.classList.add('active');
+    btn2.classList.remove('active');
+    btn3.classList.remove('active');
+    btn4.classList.remove('active');
+    content1.classList.remove('hidden');
+    content2.classList.add('hidden');
+    content3.classList.add('hidden');
+    content4.classList.add('hidden');
+}
+
+btn1.onclick = function(e){
+	e.preventDefault();
+    btn1.classList.add('active');
+    btn2.classList.remove('active');
+    btn3.classList.remove('active');
+    btn4.classList.remove('active');
+    content1.classList.remove('hidden');
+    content2.classList.add('hidden');
+    content3.classList.add('hidden');
+    content4.classList.add('hidden');
+}
+btn2.onclick = function(e){
+	e.preventDefault();
+    btn1.classList.remove('active');
+    btn2.classList.add('active');
+    btn3.classList.remove('active');
+    btn4.classList.remove('active');
+    content1.classList.add('hidden');
+    content2.classList.remove('hidden');
+    content3.classList.add('hidden');
+    content4.classList.add('hidden');
+}
+btn3.onclick = function(e){
+	e.preventDefault();
+	btn1.classList.remove('active');
+    btn2.classList.remove('active');
+    btn3.classList.add('active');
+    btn4.classList.remove('active');
+    content1.classList.add('hidden');
+    content2.classList.add('hidden');
+    content3.classList.remove('hidden');
+    content4.classList.add('hidden');
+}
+btn4.onclick = function(e){
+	e.preventDefault();
+    btn1.classList.remove('active');
+    btn2.classList.remove('active');
+    btn3.classList.remove('active');
+    btn4.classList.add('active');
+    content1.classList.add('hidden');
+    content2.classList.add('hidden');
+    content3.classList.add('hidden');
+    content4.classList.remove('hidden');
+}
---
+++ js/menu.js
@@ -0,0 +1,51 @@
+'use strict';
+
+
+let grandmenu = [
+	[
+	{menu:'LADY’S DESIGN CUT レディースデザインカット', price: 4200},
+	{menu:'MEN’S DESIGN CUT メンズデザインカット', price: 3500},
+	{menu:'FRONT CUT 前髪カット', price: 1000},
+	{menu:'EYE BLOW CUT 眉カット ＊単品予約は不可＊', price: 1000}
+	],
+ 	[
+	{menu:'COLOR カラー', price: 8400},
+	{menu:'DAMAGE LESS COLOR ダメージレスカラー', price: 10400},
+	{menu:'DESIGN COLOR デザインカラー', price: 10600},
+	{menu:'DAMAGE LESS DESIGN COLOR ダメージレスデザインカラー', price: 12600}
+ 	],
+	[
+	{menu:'PERM  パーマ', price: 9900},
+	{menu:'DAMMAGE LESS PERM  ダメージレス パーマ', price: 11900},
+	{menu:'DIGITAL PERM  デジタルパーマ', price: 10900},
+	{menu:'DAMAGE LESS DESIGN COLOR ダメージレスデザインカラー', price: 1000}
+	],
+	[
+	{menu:'TREATMENT  トリートメント', price: 3900},
+	{menu:'TREATMENT【9 STEP】', price: 5900},
+	{menu:'TREATMENT【11 STEP】＊ホームケア付き＊', price: 8900},
+	{menu:'TREATMENT【19 STEP】＊ホームケア付き＊', price: 11000}
+	],
+	[
+	{menu:'AGING CARE SPA  エイジングケアスパ', price: 4500},
+	{menu:'STRESS CARE SPA  ストレスケアスパ', price: 4500},
+	{menu:'CREME SPA  クリームスパ', price: 3500},
+	{menu:'BED CREME SPA  ベッドクリームスパ', price: 4000}
+	]
+];
+
+const tax = 1.1;
+
+
+for(let i = 0; i < grandmenu.length; i++){
+	let tr = '';
+	let array = grandmenu[i];
+	for(let i = 0; i < array.length; i++){
+		array[i].addTax = Math.floor(array[i].price * tax);
+		array[i].price = array[i].price.toLocaleString("jp",{style:"currency",currency:"JPY"});
+		array[i].addTax = `(税込${array[i].addTax.toLocaleString("jp",{style:"currency",currency:"JPY"})})`;
+		tr = tr + `<tr><td class="menu">${array[i].menu}</td><td class="price">${array[i].price} ${array[i].addTax}</td></tr>`;
+	};
+	document.getElementById('menu'+(i+1)).insertAdjacentHTML('beforeend', tr);
+};
+
